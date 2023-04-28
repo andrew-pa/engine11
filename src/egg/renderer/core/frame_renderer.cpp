ac9a638d842192473fcb3564603210fbbda886b5
Andrew Palmer <andrew.pa@outlook.com>
2023-04-28 14:27:33 -0600

    merge swap chain creation function into frame_renderer.cpp

--- src/egg/renderer/core/frame_renderer.cpp
+++ src/egg/renderer/core/frame_renderer.cpp
@@ -18,6 +18,67 @@ void frame_renderer::reset_swapchain(vk::Extent2D new_swapchain_extent) {
     init_swapchain();
 }
 
+void frame_renderer::init_swapchain() {
+    // std::cout << "swapchain extent: " << swapchain_extent.width << "x" << swapchain_extent.height
+    // << "\n";
+
+    vk::SwapchainCreateInfoKHR cfo{
+        {},
+        r->window_surface.get(),
+        r->surface_image_count,
+        r->surface_format.format,
+        r->surface_format.colorSpace,
+        swapchain_extent,
+        1,
+        vk::ImageUsageFlagBits::eColorAttachment};
+
+    if(r->graphics_queue_family_index != r->present_queue_family_index) {
+        cfo.imageSharingMode      = vk::SharingMode::eConcurrent;
+        cfo.queueFamilyIndexCount = 2;
+        uint32_t q[]              = {r->graphics_queue_family_index, r->present_queue_family_index};
+        cfo.pQueueFamilyIndices   = q;
+    } else {
+        cfo.imageSharingMode = vk::SharingMode::eExclusive;
+    }
+
+    auto surface_caps  = r->phy_dev.getSurfaceCapabilitiesKHR(r->window_surface.get());
+    cfo.preTransform   = surface_caps.currentTransform;
+    cfo.compositeAlpha = vk::CompositeAlphaFlagBitsKHR::eInherit;
+    cfo.presentMode    = vk::PresentModeKHR::eFifo;
+    cfo.clipped        = VK_TRUE;
+
+    swapchain        = r->dev->createSwapchainKHRUnique(cfo);
+    swapchain_images = r->dev->getSwapchainImagesKHR(swapchain.get());
+
+    vk::ImageViewCreateInfo ivcfo{
+        {},
+        nullptr,
+        vk::ImageViewType::e2D,
+        r->surface_format.format,
+        {},
+        vk::ImageSubresourceRange{vk::ImageAspectFlagBits::eColor, 0, 1, 0, 1}
+    };
+
+    swapchain_image_views.clear();
+    for(auto img : swapchain_images) {
+        ivcfo.image = img;
+        swapchain_image_views.emplace_back(r->dev->createImageViewUnique(ivcfo));
+    }
+
+    if(swapchain_images.size() != command_buffers.size()) {
+        command_buffers.clear();
+        command_buffers = r->dev->allocateCommandBuffersUnique(vk::CommandBufferAllocateInfo{
+            r->command_pool.get(),
+            vk::CommandBufferLevel::ePrimary,
+            (uint32_t)swapchain_images.size()});
+        command_buffer_ready_fences.clear();
+        // command buffers always start ready
+        vk::FenceCreateInfo fence_cfo{vk::FenceCreateFlagBits::eSignaled};
+        for(size_t i = 0; i < command_buffers.size(); ++i)
+            command_buffer_ready_fences.emplace_back(r->dev->createFenceUnique(fence_cfo));
+    }
+}
+
 std::vector<vk::UniqueFramebuffer> frame_renderer::create_framebuffers(
     vk::RenderPass                                                  render_pass,
     const std::function<void(size_t, std::vector<vk::ImageView>&)>& custom_image_views
